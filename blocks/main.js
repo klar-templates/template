134044893e934ae2e8386b2573e83709ce3e460a
Gabriel Lantz <gabriel.lantz@svt.se>
2022-12-21 01:23:52 +0100

    wip

--- blocks/main.js
+++ blocks/main.js
@@ -1,27 +1,27 @@
 
 import klarConfig from '../config.json';
-import Hero from "./hero";
+import BestHero from "./hero";
 import Team from "./team";
 
 const klarComponents = {
-  Hero: Hero,
-  Team: Team,
+  BestHero: BestHero,
+  // Team: Team,
 };
 
 window.klarComponents = klarComponents;
 window.klarComponentsConfig = klarConfig;
-console.log(window.klarComponents);
+// console.log(window.klarComponents);
 
 // const blocks = klarConfig.data_defaults.blocks;
 // console.log(klarConfig)
 
-ReactDOM.createRoot(
-  document.body,
-).render(
-  <React.StrictMode>
-    <>
-      <Hero />
-      <Team />
-    </>
-  </React.StrictMode>,
-);
+// ReactDOM.createRoot(
+//   document.body,
+// ).render(
+//   <React.StrictMode>
+//     <>
+//       <BestHero />
+//       {/* <Team /> */}
+//     </>
+//   </React.StrictMode>,
+// );
