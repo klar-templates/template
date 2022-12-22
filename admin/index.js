2b4bad20de31d0bca5668511d1f65b2c72f432a2
Gabriel Lantz <gabriel.lantz@svt.se>
2022-12-22 23:18:03 +0100

    wip

--- admin/index.js
+++ admin/index.js
@@ -7,8 +7,11 @@ function clearBreakpoints() {
   iframe.classList.remove('desktop');
   iframe.classList.remove('ipad');
   iframe.classList.remove('mobile');
+  desktop.classList.remove('active');
+  ipad.classList.remove('active');
+  mobile.classList.remove('active');
 }
 
-desktop.addEventListener('click', (e) => {if(iframe.classList.contains('desktop')) {clearBreakpoints();} else {clearBreakpoints();iframe.classList.add('desktop')}});
-ipad.addEventListener('click', (e) => {if(iframe.classList.contains('ipad')) {clearBreakpoints();} else {clearBreakpoints();iframe.classList.add('ipad')}});
-mobile.addEventListener('click', (e) => {if(iframe.classList.contains('mobile')) {clearBreakpoints();} else {clearBreakpoints();iframe.classList.add('mobile')}});
+desktop.addEventListener('click', (e) => {if((window.innerWidth - 260) < 1280) {clearBreakpoints();return};if(iframe.classList.contains('desktop')) {clearBreakpoints();} else {clearBreakpoints();iframe.classList.add('desktop');e.target.classList.add('active');}});
+ipad.addEventListener('click', (e) => {if((window.innerWidth - 260) < 1024) {clearBreakpoints();return};if(iframe.classList.contains('ipad')) {clearBreakpoints();} else {clearBreakpoints();iframe.classList.add('ipad');e.target.classList.add('active');}});
+mobile.addEventListener('click', (e) => {if((window.innerWidth - 260) < 768) {clearBreakpoints();return};if(iframe.classList.contains('mobile')) {clearBreakpoints();} else {clearBreakpoints();iframe.classList.add('mobile');e.target.classList.add('active');}});
