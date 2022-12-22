23168df2594799d5bc75c1e3d2d8ca0880eff18b
Gabriel Lantz <gabriel.lantz@svt.se>
2022-12-22 12:38:02 +0100

    wip

--- blocks/index.js
+++ blocks/index.js
@@ -13,13 +13,46 @@ window.templateConfig = templateConfig;
 
 // Add Klar react app script after Babel has transpiled the JSX code
 const script = document.createElement('script');
-script.src = 'http://localhost:4173/assets/index.ebcd8e4e.js';
+script.src = 'http://localhost:4173/assets/index.b21a2e21.js';
 script.type = 'module';
 script.crossOrigin = true;
 document.querySelector('head').appendChild(script);
 
+window.initTemplate = function (data) {
+  console.log(data);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // const blocks = klarConfig.data_defaults.blocks;
-// console.log(klarConfig)
 
 // ReactDOM.createRoot(
 //   document.body,
