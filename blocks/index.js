34cffa5b276994deb8ef928b97bcd5354ef7095e
Gabriel Lantz <gabriel.lantz@svt.se>
2022-12-22 12:56:43 +0100

    wip

--- blocks/index.js
+++ blocks/index.js
@@ -11,56 +11,16 @@ const templateComponents = {
 window.templateComponents = templateComponents;
 window.templateConfig = templateConfig;
 
-// Add Klar react app script after Babel has transpiled the JSX code
+// Add klar-pages-app script after Babel has transpiled the JSX code
 const script = document.createElement('script');
-script.src = 'http://localhost:4173/assets/index.b21a2e21.js';
+script.src = 'http://localhost:4173/assets/index.45a9d7ad.js';
 script.type = 'module';
 script.crossOrigin = true;
 document.querySelector('head').appendChild(script);
 
 window.initTemplate = function (data) {
-  console.log(data);
+  const startpage = data.data.pages[0];
+  const blocks = startpage.blocks;
+  const hero = blocks[0];
+  console.log(hero);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const blocks = klarConfig.data_defaults.blocks;
-
-// ReactDOM.createRoot(
-//   document.body,
-// ).render(
-//   <React.StrictMode>
-//     <>
-//       <BestHero />
-//       {/* <Team /> */}
-//     </>
-//   </React.StrictMode>,
-// );
