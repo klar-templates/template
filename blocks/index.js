7edf9b80a27b0a2b90d3d4c0b2d189c381071206
Gabriel Lantz <gabriel.lantz@svt.se>
2022-12-24 14:00:54 +0100

    wip

--- blocks/index.js
+++ blocks/index.js
@@ -1,10 +1,10 @@
 
-import Hero from "./hero";
-import Team from "./team";
+import hero from "./hero";
+import team from "./team";
 
 const templateComponents = {
-  Hero: Hero,
-  Team: Team,
+  TemplateEditorHero: hero,
+  TemplateEditorTeam: team,
 };
 
 window.templateComponents = templateComponents;
